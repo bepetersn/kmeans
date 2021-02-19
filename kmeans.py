bb292dc7dce683454cdc860441a2a22654174c8d
Brian Peterson <bepetersn@gmail.com>
2021-02-18 17:04:21 -0700

    Finished kmeans

--- kmeans.py
+++ kmeans.py
@@ -6,28 +6,75 @@ import math
 import random
 import numpy as np
 from pprint import pprint
+import matplotlib
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 
-GOOD_ENOUGH = 0.5
+GOOD_ENOUGH = 0.0001
+NUM_CLUSTERS = 3
 
 def main():
     
-    if len(sys.argv) < 2:
-        print("Usage: kmeans.py somedata.csv")
+    if len(sys.argv) != 2:
+        print("Usage: ./kmeans.py somedata.csv")
         exit(1)
 
     data = []
-    with open(sys.argv[1]) as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if (row[0] and row[1] and row[2] and row[3]):
-                data.append(np.array([
-                    float(row[1]), 
-                    float(row[2]), 
-                    float(row[3])
-                ]))
 
-    pprint(data)
-    k_means(data, 3)
+    with open(sys.argv[1]) as f:
+        csv_reader = csv.reader(f)
+        row_iter = iter(csv_reader)
+        next(row_iter) # Skip the first row
+        for row in row_iter:
+            if (row[0] and row[1] and row[2] and row[3]):
+                data.append(_parse_vector(row))
+            else:
+                break
+
+        next(row_iter) # Skip the header in this row
+        initial_centers = [ # Grab the last 3 rows
+            _parse_vector(next(row_iter)), 
+            _parse_vector(next(row_iter)), 
+            _parse_vector(next(row_iter))
+        ]
+
+    # Convert the data to a 600x3 matrix
+    np_data = np.array(data)
+
+    # Run the k-means algorithm to get the
+    # group assignments
+    groups = k_means(np_data, NUM_CLUSTERS, initial_centers)
+
+    # Segregate the vectors into groups by the group 
+    # assignments we generated above
+    grouped_vectors = []
+    for group_id in range(0, NUM_CLUSTERS):
+        grouped = [v for (i, v) in enumerate(np_data) \
+                     if groups[i] == group_id]
+        grouped_vectors.append(grouped)
+            
+    # Start plotting
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    colors = ['red', 'green', 'blue']
+
+    for i, vector_group in enumerate(grouped_vectors):
+        np_vector_group = np.array(vector_group)
+        np_vector_group_t = np_vector_group.transpose()
+        ax.scatter(
+            np_vector_group_t[0], 
+            np_vector_group_t[1], 
+            np_vector_group_t[2], color=colors[i])
+    
+    # Finish plotting
+    matplotlib.use('TkAgg')
+    pyplot.show()
+
+
+def _parse_vector(row): 
+    return [
+        float(row[1]), float(row[2]), float(row[3])
+    ]
 
 
 def get_norm(vector):
@@ -49,6 +96,20 @@ def get_vectors_average(vectors):
 
 
 def get_closest_representative(vector, reps):
+    """Find the closest representative to a vector 
+       from among representatives and return its 
+       index in reps. 
+
+    Args:
+        vector (numpy.array): A vector to compare
+        against the representative vectors in reps
+        reps (list[numpy.array]): A list of vectors,
+        to be compared against vector
+
+    Returns:
+        int: Return the index of the closest 
+        representative vector within reps
+    """
     min = math.inf
     min_i = None
     for i, rep in enumerate(reps):
@@ -59,7 +120,7 @@ def get_closest_representative(vector, reps):
     return min_i
 
 
-def j_clust(vectors, groups, reps):
+def get_j_clust(vectors, groups, reps):
     value = 0
     for i, vector in enumerate(vectors):
         # note: vector op
@@ -67,23 +128,36 @@ def j_clust(vectors, groups, reps):
     return value / len(vectors)
 
 
-def k_means(vectors, k, reps=None):
-    # Given a list of n vectors, xi, … , xn, and k, the number of groups to form
-    # Choosing initial group representatives is optional
-    if reps is None:
-        reps = vectors[0:k+1]
+def k_means(vectors, k, reps):
+    # Given a list of n vectors, xi, … , xn, and k, the number of 
+    # groups to form, and reps, the initial choice of vector 
+    # representatives fo each group / cluster
+
     # Choose initial groups for each vector
-    groups = [random.randint(1, k) for _ in vectors]
+    groups = list(range(0, k-1))
+    group_assignments = [random.randint(0, k-1) for _ in vectors]
+    old_j_clust = j_clust = math.inf
     while True:
-        for vector, i in enumerate(vectors):
-            groups[i] = get_closest_representative(vector, reps)
-            reps[i] = get_vectors_average(vectors)
-        if j_clust(vectors, groups, reps) <= GOOD_ENOUGH:
+        print("group assignments: {}".format(group_assignments))
+        print("reps: {}".format(reps))
+        print("j_clust: {}".format(j_clust))
+        for i, vector in enumerate(vectors):
+            group_assignments[i] = \
+                get_closest_representative(vector, reps)
+        for group in groups:
+            vector_indices_in_grp = \
+                [i for (i, g) in enumerate(group_assignments) \
+                        if g == group]
+            reps[group] = get_vectors_average(
+                [v for (i, v) in enumerate(vectors) \
+                        if i in vector_indices_in_grp]
+            )
+        old_j_clust = j_clust
+        j_clust = get_j_clust(vectors, group_assignments, reps)
+        if (old_j_clust - j_clust) <= GOOD_ENOUGH:
+            print("diff: {}".format(old_j_clust - j_clust))
             break
-    return groups
-
-
-            
+    return group_assignments
       
 
 if __name__ == "__main__":
